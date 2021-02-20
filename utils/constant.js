e1015ac00c592014861be63017bd57eefa3c49b0
somoye123 <somoye.ayotunde@gmail.com>
2021-02-20 22:47:52 +0100

    add getPagingData to constant module

--- utils/constant.js
+++ utils/constant.js
@@ -5,6 +5,15 @@ const getPagination = (page, size) => {
   return { limit, offset };
 };
 
+const getPagingData = (data, page, limit) => {
+  const { count: totalItems, rows: tutorials } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  return { totalItems, tutorials, totalPages, currentPage };
+};
+
 module.exports = {
   getPagination,
+  getPagingData,
 };
