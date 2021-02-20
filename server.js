ce7549ece88e312156c767aa6061ad28cbd55ca8
somoye123 <somoye.ayotunde@gmail.com>
2021-02-20 17:18:19 +0100

    include route in sever.js

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to somoye application.' });
 });
 
+require('./routes/turorial.routes.js')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
